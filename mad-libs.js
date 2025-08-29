abba90321ec783d1c68960bf2bc4a059f34666d3
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-08-29 18:24:08 +0200

    Initial commit

---
+++ mad-libs.js
@@ -0,0 +1,5 @@
+let userName = prompt("What's your name?");
+let adjective = prompt("What's your favorite adjective?");
+let noun = prompt("Pick a noun");
+
+console.log(`I saw the ${adjective} ${noun} named ${userName}`);
