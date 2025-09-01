74af2e7f9d0df9e73bb10e814a8df6236e43b21b
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-09-01 19:55:38 +0200

    Add new exercise

---
+++ 3. loops-and-iteration/print-numbers-do-while-loop.js
@@ -0,0 +1,7 @@
+let count = 1;
+do {
+    console.log(count);
+    count++;
+} while (count <= 5);
+
+// 1, 2, 3, 4, 5
---
+++ 3. loops-and-iteration/print-numbers-while-loop.js
@@ -0,0 +1,8 @@
+let count = 1;
+
+while (count <= 5) {
+    console.log(count);
+    count++;
+}
+
+// 1, 2, 3, 4, 5
