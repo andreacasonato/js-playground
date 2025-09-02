c26c2b98537cee3628046cef37aa10869e8fdc1d
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-09-02 09:54:57 +0200

    Add new exercise

---
+++ 3. loops-and-iteration/factorial-calculator.js
@@ -0,0 +1,11 @@
+let finalResult = 1;
+
+for (i = 1; i <= 10; i++) {
+    finalResult *= i;
+}
+
+console.log(finalResult)
+
+// Calculating the factorial of 10
+// 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+// Result: 3628800
