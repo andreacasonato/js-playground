0158282c21276712b1a47c7e8b1286de27e12a07
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-09-10 08:24:08 +0200

    Add new exercise

---
+++ 5. arrays-objects-dom-fundamentals/5.3 dom/page-header-updater.js
@@ -0,0 +1,6 @@
+// 1. Select the element and store it in a variable.
+// This variable now holds a reference to the h1 element itself.
+let mainTitle = document.getElementById('main-title');
+
+// 2. Use the variable to change the element's text content.
+mainTitle.textContent = "DOM Manipulation is Fun!";
