093f5af401145b116047948f1d85e90a0d5dc2ac
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-09-11 08:22:15 +0200

    Add new exercise

--- 1. Fundamentals and Basics/type-coercion-type-conversion.js
+++ 1. Fundamentals and Basics/type-coercion-type-conversion.js
@@ -4,7 +4,7 @@ let myString = '20';
 let sumString = myNumber + myString;
 console.log(sumString); // 1020
 
-let convertNumber = parseInt('20');
+let convertNumber = parseInt(myString);
 
 let sumNumber = myNumber + convertNumber;
 console.log(sumNumber); // 30
