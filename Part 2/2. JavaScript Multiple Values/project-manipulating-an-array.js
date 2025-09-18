0d457b251fafdb5398bf51102fecef44156bf69c
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-09-18 11:02:12 +0200

    Add new exercise

--- Part 2/2. JavaScript Multiple Values/project-manipulating-an-array.js
+++ Part 2/2. JavaScript Multiple Values/project-manipulating-an-array.js
@@ -13,3 +13,27 @@
 // Assign MIDDLE to the third index value.
 // Add LAST to the last position in the array.
 // Output the final array to the console.
+
+const thelist = ['laurence', 'Svekis', true, 35, null, undefined, { test: 'one', score: 55 }, ['one', 'two']];
+
+// Remove first item of the array
+thelist.shift();
+
+// Remove last item of the array
+thelist.pop();
+
+// Add FIRST to the start of the array
+thelist.unshift('FIRST');
+
+// Assign hello World to the fourth item value.
+thelist[3] = 'hello World';
+
+// Assign MIDDLE to the third index value.
+thelist[2] = 'MIDDLE';
+
+// Add LAST to the last position in the array.
+thelist.push('LAST');
+
+// Output the final array to the console.
+console.log(thelist);
+// ["FIRST", "Svekis", "MIDDLE", "hello World", "LAST"]
