20dd8efcad608e6634e966220951e4ea2f592abc
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-09-19 15:31:25 +0200

    Add new exercise

--- Part 3/1. Fundamentals and Basics/exercise-2.js
+++ Part 3/1. Fundamentals and Basics/exercise-2.js
@@ -2,3 +2,13 @@
 // Use prompt() to get a number from a user. 
 // Calculate the square root of that number and round it to the nearest integer. 
 // Alert the result to the user.
+
+let input = prompt('Pick a random number');
+
+let number = parseFloat(input);
+
+let squareRoot = Math.sqrt(number);
+
+let rounded = Math.round(squareRoot);
+
+alert(rounded);
