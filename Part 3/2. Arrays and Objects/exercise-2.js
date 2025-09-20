ebacbc814c9481c521f975b8bd75eb044e480eea
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-09-20 14:55:12 +0200

    Add new exercise

--- Part 3/2. Arrays and Objects/exercise-2.js
+++ Part 3/2. Arrays and Objects/exercise-2.js
@@ -1,3 +1,12 @@
 // (Easy) 
 // Create an object book with properties title, author, yearPublished, and isAvailable (boolean). 
 // Log a sentence using dot notation: "The book [title] by [author] was published in [year]."
+
+let book = {
+    title: 'Economics in one lesson',
+    author: 'Henry Hazlitt',
+    yearPublished: 1988,
+    isAvailable: true,
+}
+
+console.log(`The book ${book.title} by ${book.author} was published in ${book.yearPublished}.`);
--- Part 3/2. Arrays and Objects/exercise-3.js
+++ Part 3/2. Arrays and Objects/exercise-3.js
@@ -4,3 +4,24 @@
 // b) Use find() to get the first number greater than 12.
 // c) Use map() to create a new array where each number is doubled.
 // Log all results.
+
+function greaterThan(num) {
+    return num > 10;
+}
+let arrayNums = [10, 5, 8, 20, 3, 15].filter(greaterThan);
+console.log(arrayNums);
+
+
+function moreThan(number) {
+    return number > 12;
+}
+let nums = [10, 5, 8, 20, 3, 15].find(moreThan);
+console.log(nums);
+
+
+function double(numbr) {
+    return numbr * 2;
+}
+
+let newArray = [10, 5, 8, 20, 3, 15].map(double);
+console.log(newArray);
