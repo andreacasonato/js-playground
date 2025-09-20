dae17bac4d1ee3e8efa4c3ca3de00c59d99088e7
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-09-20 14:26:21 +0200

    Add new exercise

--- Part 3/2. Arrays and Objects/exercise-1.js
+++ Part 3/2. Arrays and Objects/exercise-1.js
@@ -2,3 +2,13 @@
 // Create an array of your top 5 favorite movies. 
 // Log the first and last movie. Add a new movie to the end. 
 // Remove the first movie. Log the final array.
+
+let movies = ['the mask', 'spiderman', 'parasite', 'toy story', 'harry potter'];
+
+console.log(movies[0], movies[movies.length - 1]);
+
+movies.push('shrek');
+
+movies.shift();
+
+console.log(movies);
