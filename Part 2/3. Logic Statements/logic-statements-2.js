586f1b51afbaa2d2537b6eae05d069df4bcaa0ff
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-09-21 11:58:43 +0200

    Add new exercise

--- Part 2/3. Logic Statements/logic-statements-2.js
+++ Part 2/3. Logic Statements/logic-statements-2.js
@@ -5,3 +5,17 @@
 // If the input age is equal to or greater than 19, set the message variable to confirm entry to the venue but deny the purchase of alcohol.
 // Provide a default else statement to set the message variable to deny entry if none are true.
 // Output the response message variable to the console.
+
+let question = prompt('How old are you?');
+
+let userAge = Number(question);
+
+console.log(userAge);
+
+if (userAge >= 21) {
+    console.log('You may enter the venue and purchase alcohol');
+} else if (userAge >= 19) {
+    console.log('You may enter the venue but you are not allowed to purchase any alcohol');
+} else {
+    console.log('You won\'t be allowed entry today');
+}
