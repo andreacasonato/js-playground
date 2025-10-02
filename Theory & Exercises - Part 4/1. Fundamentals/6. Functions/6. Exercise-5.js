b9bcac8743d5085d068b79a0f27cc373053f67a5
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-10-02 17:37:11 +0200

    Add new exercise

--- Theory & Exercises - Part 4/1. Fundamentals/6. Functions/6. Exercise-5.js
+++ Theory & Exercises - Part 4/1. Fundamentals/6. Functions/6. Exercise-5.js
@@ -4,3 +4,53 @@
 // Find the longest string in an array
 // Check if an array contains a specific value
 
+
+// Return the first element of an array
+
+function getFirstElement(array1) {
+    return array1[0];
+}
+
+let array1 = ["apple", "banana", "kiwi"];
+
+console.log(getFirstElement(array1));
+
+
+// Return the last element of an array
+
+function getLastElement(array2) {
+    return array2[array2.length - 1];
+}
+
+let array2 = ["watermelon", "pear", "mango"];
+
+console.log(getLastElement(array2));
+
+
+// Find the longest string in an array
+
+function longestString(array3) {
+    let longest = ""
+    for (let str of array3) {
+        if (str.length > longest.length) {
+            longest = str;
+        }
+    }
+    return longest;
+}
+
+let array3 = ["pineapple", "apricot", "banana", "mangosteen"];
+
+console.log(longestString(array3));
+
+
+// Check if an array contains a specific value
+
+
+function arrayContains(array4, value) {
+    return array4.includes(value);
+}
+
+let array4 = ["pineapple", "apricot", "banana", "mangosteen"];
+console.log(arrayContains(array4, "apricot"));
+console.log(arrayContains(array4, "kiwi"));
--- Theory & Exercises - Part 4/1. Fundamentals/6. Functions/8. Exercise-7.js
+++ Theory & Exercises - Part 4/1. Fundamentals/6. Functions/8. Exercise-7.js
@@ -1,5 +1,5 @@
 // Create functions that:
-//Calculate total price (price × quantity)
-//Create a user profile with validation
-//Format a date string from day, month, year
-//Calculate body mass index (BMI)
+// Calculate total price (price × quantity)
+// Create a user profile with validation
+// Format a date string from day, month, year
+// Calculate body mass index (BMI)
