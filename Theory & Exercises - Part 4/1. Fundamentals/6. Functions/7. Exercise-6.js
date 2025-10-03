f8d40380314f53ce593a7a9dd7dc436d28bdc6a8
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-10-03 14:36:52 +0200

    Add new exercise

--- Theory & Exercises - Part 4/1. Fundamentals/6. Functions/7. Exercise-6.js
+++ Theory & Exercises - Part 4/1. Fundamentals/6. Functions/7. Exercise-6.js
@@ -3,3 +3,41 @@
 // Return the full name from a person object
 // Check if a person is an adult (18+)
 // Create a product object with name, price, and inStock
+
+
+// Create a person object from name and age
+function createPerson(firstName, lastName, age) {
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+    };
+}
+
+// Return the full name from a person object
+function getFullName(person) {
+    return `${person.firstName} ${person.lastName}`;
+}
+
+// Check if a person is an adult (18+)
+function isAdult(person) {
+    return person.age >= 18;
+}
+
+// Create a product object with name, price, and inStock
+function createProduct(itemName, itemPrice, inStock) {
+    return {
+        itemName: itemName,
+        itemPrice: itemPrice,
+        inStock: inStock,
+    }
+};
+
+
+// Example usage:
+const p1 = createPerson("Andrew", "Doe", 20);
+console.log(getFullName(p1));
+console.log(isAdult(p1));
+
+const product1 = createProduct("iPhone", 500, true);
+console.log(product1);
--- Theory & Exercises - Part 4/1. Fundamentals/6. Functions/8. Exercise-7.js
+++ Theory & Exercises - Part 4/1. Fundamentals/6. Functions/8. Exercise-7.js
@@ -3,3 +3,56 @@
 // Create a user profile with validation
 // Format a date string from day, month, year
 // Calculate body mass index (BMI)
+
+
+// Calculate total price (price × quantity)
+
+function totalPrice(price, quantity) {
+    return price * quantity;
+}
+
+console.log(totalPrice(20, 30));
+
+
+// Create a user profile with validation
+function createUserProfile(username, email, age) {
+    if (!username || username.length < 3) {
+        return "Error: Username must be at least 3 characters long.";
+    }
+    if (!email.includes("@")) {
+        return "Error: Invalid email address.";
+    }
+    if (age < 0) {
+        return "Error: Age must be positive.";
+    }
+
+    // If valid, return the user profile object
+    return {
+        username,
+        email,
+        age
+    };
+}
+
+console.log(createUserProfile("Alex", "alex@mail.com", 25));
+
+
+// Format a date string from day, month, year
+function formatDate(day, month, year) {
+    const d = String(day).padStart(2, "0");
+    const m = String(month).padStart(2, "0");
+    return `${d}/${m}/${year}`;
+}
+
+console.log(formatDate(3, 10, 2025));
+
+
+// Calculate body mass index (BMI)
+function calculateBMI(weightKg, heightMeters) {
+    if (heightMeters <= 0) {
+        return "Error: Height must be greater than 0.";
+    }
+    const bmi = weightKg / (heightMeters ** 2);
+    return bmi.toFixed(2);
+}
+console.log(calculateBMI(65, 1.75));
